446153aa4de784cb3fabd7a5c0ea0dc4afa58ae3
karshann <karshan951@gmail.com>
2020-05-04 20:06:19 +0200

    ajout de rectangle

---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+public class Rectangle implements Forme{
+    String nom;
+    Point p1;
+    Point p2;
+    public Rectangle(String nom , int x1 , int y1 , int x2 , int y2){
+        this.p1=new Point(x1,y1);
+        this.p2=new Point(x2,y2);
+        this.nom = nom;
+    }
+
+    @Override
+    public void move(double x, double y) {
+        this.p1.sommePoint(x,y);
+        this.p2.sommePoint(x,y);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "nom='" + nom + '\'' +
+                ", p1=" + p1.toString() +
+                ", p2=" + p2.toString() +
+                '}';
+    }
+
+    @Override
+    public void print() {
+        System.out.println(this.toString());
+    }
+}
